bd3eb6bf10a947e327ebee47246813ca7097fc31
Eli <118321389+Hamptonix@users.noreply.github.com>
2026-05-20 08:50:28 -0700

    Update icon URL for The Gatsby Derby game

--- games/games.js
+++ games/games.js
@@ -4,7 +4,7 @@ const GAMES = [
     title: "The Gatsby Derby",
     tag: "Arcade",
     desc: "My School English Final",
-    icon: "/assets/gatsby.jpg",
+    icon: "https://hamptonix.com/assets/gatsby.jpg",
     url: "https://hamptonix.com/games/localgames/gatsbyderby.html",
     category: "Arcade"
   },
